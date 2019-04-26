dd4a4995c9c7c52db122f9f4cecf7d5128e0b742
xw <23066125+xwang1000@users.noreply.github.com>
2019-04-25 20:27:02 -0700

    Add and display themes

--- src/ThemePanel.js
+++ src/ThemePanel.js
@@ -1,19 +1,40 @@
 import React from 'react'
-import { atelierEstuaryDark, atelierEstuaryLight, vs2015, xcode, rainbow
+import { atelierEstuaryDark, atelierEstuaryLight, vs2015, xcode, rainbow, solarizedDark, solarizedLight, purebasic
 } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+import './ThemePanel.css'
+
+const options = {
+  'none': {},
+  'atelierEstuaryDark': atelierEstuaryDark,
+  'atelierEstuaryLight': atelierEstuaryLight,
+  'vs2015': vs2015,
+  'xcode': xcode,
+  'rainbow': rainbow,
+  'solarizedDark': solarizedDark,
+  'solarizedLight': solarizedLight,
+  'purebasic': purebasic
+}
 
 const ThemePanel = props => {
   const { setTheme } = props
-  
-  const renderThemeOptions = (
-    <p>
-      <span onClick={() => setTheme(atelierEstuaryLight)}>atelierEstuaryLight</span> | 
-      <span onClick={() => setTheme(vs2015)}>vs2015</span> | 
-      <span onClick={() => setTheme(xcode)}>xcode</span>
-    </p>
-  )
 
-  return renderThemeOptions
+  const renderThemeOptions = () => {
+    const optionBlocks = []
+    for (let name in options) {
+      optionBlocks.push(
+        <div className="theme-panel__block" onClick={() => setTheme(options[name])}>{name}</div>
+      )
+    }
+
+    return optionBlocks
+  }
+
+  return (
+    <div className="theme-panel">
+      <p>theme</p>
+      {renderThemeOptions()}
+    </div>
+  )
 }
 
 export default ThemePanel
